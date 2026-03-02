09bb9310bd258099ffb312437d39dbd7d8fc6ff9
lagirebahan <cj.lagirebahan@gmail.com>
2026-03-02 10:43:57 +0700

    routerfix1
    fix app router, navbar logo fix

---
+++ my-app/app/login/page.tsx
@@ -0,0 +1,26 @@
+import MainNavbar from "@/components/layout/Navbar";
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-[#2e2e2e] to-[#b6a88b] p-8">
+      
+      {/* sementara title pagenya gini dulu, blm ada navbar */}
+      <h1 className="text-white text-2xl mb-6">Login Page</h1>
+
+      <div>Testing loginpage</div>
+
+      
+      
+    </div>
+  )
+}
+
+// /* reusable component line 34 */
+
+// function StatusRow({ label, count }: { label: string; count: number }) {
+//   return (
+//     <div className="flex justify-between items-center">
+//       <span className="text-gray-700">{label}</span>
+//       <span className="text-red-500 font-semibold">{count}</span>
+//     </div>
+//   )
+// }
