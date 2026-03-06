b466be770cbd6c9a7f72e04e504cad1f97209e21
lagirebahan <cj.lagirebahan@gmail.com>
2026-03-06 19:24:14 +0700

    routerfix2
    teams router

---
+++ my-app/app/teams/[teamId]/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default async function TeamDetail({ params }: { params: Promise<{ teamId: string }>;}) {
+    const { teamId } = await params;
+    return (
+        <div>
+            <h1>Team ID: {teamId}</h1>
+
+            <Link href={`/teams/${teamId}/tasks`}>Tasks</Link>
+
+            <Link href={`/teams/${teamId}/verification`}>Verification</Link>
+        </div>
+  );
+}
--- my-app/app/teams/page.tsx
+++ my-app/app/teams/page.tsx
@@ -1,18 +1,37 @@
 import MainNavbar from "@/components/layout/Navbar";
+import Link from "next/link";
+
+const teams = [
+  { id: "team1", name: "Team Alpha" },
+  { id: "team2", name: "Team Beta" },
+  { id: "team3", name: "Team Gamma" },
+];
+
+
 export default function TeamPage() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#2e2e2e] to-[#b6a88b] p-8">
       
       {/* sementara title pagenya gini dulu, blm ada navbar */}
       <h1 className="text-white text-2xl mb-6">Team Page</h1>
-
-      <div>Testing teampage</div>
+      
+      <ul>
+        {teams.map((team) => (
+          <li key={team.id}>
+            <Link href={`/teams/${team.id}`}>
+              {team.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      
 
       
       
     </div>
   )
 }
+
 
 // /* reusable component line 34 */
 
